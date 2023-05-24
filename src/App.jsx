16f3dd8f40f2efd71c4c691c9a9e6bd2b6c9666c
Rishav Jindal <rishav.jindal@blackngreen>
2023-05-24 16:35:20 +0530

    changes in cards on home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import "./App.css";
-import { Card,Header } from "./CommonComponent";
+import { Card, Header } from "./CommonComponent";
 
 function App() {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className="App p-4 ">
+    <div className="App w-full">
       <Header />
-      <div className="flex flex-wrap justify-center my-10 mx-4">
-      <Card item={"XGame"} style={{ backgroundColor: 'red' }} handleClick={()=>navigate("/xgame")} />
-      <Card item={"Swipe4win"} style={{ backgroundColor: 'green' }} handleClick={()=>navigate("/swipe4win")}  />
-      <Card item ={"Christianity"} style={{ backgroundColor: 'orange' }} handleClick={()=>navigate("/christianity")}  />
-      <Card item ={"Ibadat"} style={{ backgroundColor: 'purple' }} handleClick={()=>navigate("/ibadat")}  />
-      <Card item ={"Education-Portal"} style={{ backgroundColor: 'yellow' }} handleClick={()=>navigate("/educ")}  />
+      <div className="flex justify-center items-center w-full mt-10">
+      <div className="bg-white rounded-md drop-shadow-2xl w-[90%] h-[300px]">
+        <div className="text-black py-5 px-5">All Projects</div>
+        <div className="flex flex-wrap justify-center my-1 mx-4 ">
+          <Card
+            item={"XGame"}
+            style={{ backgroundColor: "rgb(217, 251, 255)" }}
+            handleClick={() => navigate("/xgame")}
+          />
+          <Card
+            item={"Swipe4win"}
+            style={{ backgroundColor: "rgb(255, 253, 195)" }}
+            handleClick={() => navigate("/swipe4win")}
+          />
+          {/* <Card item ={"Christianity"} style={{ backgroundColor: 'orange' }} handleClick={()=>navigate("/christianity")}  /> */}
+          <Card
+            item={"Ibadat"}
+            style={{ backgroundColor: "rgb(255, 225, 225)" }}
+            handleClick={() => navigate("/ibadat")}
+          />
+          <Card
+            item={"Education-Portal"}
+            style={{ backgroundColor: "rgb(222, 246, 232)" }}
+            handleClick={() => navigate("/educ")}
+          />
+        </div>
       </div>
-     
+    </div>
     </div>
   );
 }
--- src/CommonComponent/Cards.jsx
+++ src/CommonComponent/Cards.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '35vh',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "35vh",
   },
   paper: {
-    padding: theme.spacing(12),
-     margin:theme.spacing(4,1),
-    textAlign: 'center',
+    width: "200px",
+    height: "100px",
+    // padding: theme.spacing(12),
+    margin: theme.spacing(4, 1),
+    textAlign: "center",
   },
 }));
 
 export default function Card(props) {
   const classes = useStyles();
-const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper} style={props.style} onClick={props.handleClick}>
-        <h1 className='text-xl font-semibold ' >{props.item}</h1>
+      <Paper
+        className={classes.paper}
+        style={props.style}
+        onClick={props.handleClick}
+      >
+        <h1 className="text-xl font-semibold ">{props.item}</h1>
       </Paper>
     </div>
   );
