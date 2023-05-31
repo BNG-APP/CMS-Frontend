e8f3c73b22bfa6639d4a227c2bcd5c8622631484
Sonali.Jain <sonali.jain@blackngreen.com>
2023-05-31 15:19:26 +0530

    calendar added

--- src/Component/Swipe4win/ViewResult.jsx
+++ src/Component/Swipe4win/ViewResult.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers"; 
+import DateFnsUtils from "@date-io/date-fns";
 
 function ViewResult() {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
   return (
-    <div>ViewResult</div>
+    <div>
+       <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <div>
+        <DatePicker
+          label="Start Date"
+          value={startDate}
+          onChange={handleStartDateChange}
+          // Add any desired props, such as format or disablePast, to customize the DatePicker
+        />
+        <DatePicker
+          label="End Date"
+          value={endDate}
+          onChange={handleEndDateChange}
+          // Add any desired props, such as format or disablePast, to customize the DatePicker
+        />
+      </div>
+    </MuiPickersUtilsProvider>
+    </div>
   )
 }
 
