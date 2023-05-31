ba9b24ea7d01c1839d3104eaf8c99b6b864116d4
Rishav Jindal <rishav.jindal@blackngreen>
2023-05-31 14:57:36 +0530

    edit detail changes

--- src/Component/Swipe4win/EditDetails.jsx
+++ src/Component/Swipe4win/EditDetails.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import {  useLocation, useNavigate, useParams } from "react-router-dom";
 import { Button, TextField } from "@material-ui/core";
+import { convertToPNG } from "../../CommonComponent/CovertToPNG";
 
 const EditEntity = () => {
   const history = useNavigate();
   const location = useLocation();
   const [formData, setFormData] = useState({});
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [convertedImage,setConvertedImage] = useState(null);
   const rowData = location.state;
   console.log("row data",rowData);
   const { id } = useParams();
@@ -30,18 +33,37 @@ const EditEntity = () => {
    // history.push("/view");
   };
 
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+    convertToPNG(file, setConvertedImage);
+  };
+
   return (
     <div>
       <h2>Edit Row</h2>
       <form onSubmit={handleSubmit}>
-        <TextField
+        <img
           label="Image URL"
           name="imageUrl"
-          value={formData.imageUrl || ""}
+          src={selectedImage?convertedImage:formData.imageUrl || ""}
           onChange={handleInputChange}
           fullWidth
           margin="normal"
         />
+       { console.log("converted image",convertedImage)}
+        <input
+        accept="image/*"
+        type="file"
+        id="select-image"
+        style={{ display: "none" }}
+        onChange={handleImageChange}
+      />
+      <label htmlFor="select-image">
+        <Button variant="contained" color="primary" component="span">
+          Upload Image
+        </Button>
+      </label>
         <TextField
           label="Question Text (Decoded)"
           name="questionText"
@@ -54,7 +76,7 @@ const EditEntity = () => {
         <TextField
           label="Answer Option 1"
           name="answerOption1"
-          value={formData.answerOption1 || ""}
+          value={formData.answerOption?.option1 || ""}
           onChange={handleInputChange}
           fullWidth
           margin="normal"
@@ -62,7 +84,39 @@ const EditEntity = () => {
         <TextField
           label="Answer Option 2"
           name="answerOption2"
-          value={formData.answerOption2 || ""}
+          value={formData.answerOption?.option2 || ""}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Level"
+          name="level"
+          value={formData.level|| ""}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Priority"
+          name="priority"
+          value={formData.priority || ""}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Answer"
+          name="answer"
+          value={formData.answer || ""}
+          onChange={handleInputChange}
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          label="Operator Id"
+          name="operatorId"
+          value={formData.operatorId || ""}
           onChange={handleInputChange}
           fullWidth
           margin="normal"
