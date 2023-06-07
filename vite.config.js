07ac4910364ab740ccba3a421c5b1d8bcac03d4f
Sonali.Jain <sonali.jain@blackngreen.com>
2023-06-07 17:57:41 +0530

    fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react()],
+// })
+module.exports = defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build'
+  }
+});
+
+
