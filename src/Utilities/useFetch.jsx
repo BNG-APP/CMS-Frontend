28f33bcd84f82ef02af428fbd6b515291f4f3491
Sonali.Jain <sonali.jain@blackngreen.com>
2023-06-07 18:38:35 +0530

    remove log

--- src/Utilities/useFetch.jsx
+++ src/Utilities/useFetch.jsx
@@ -5,7 +5,6 @@ const useFetch = (url,data) => {
   const [loading, setLoading] = useState(false)
 
   const apiCall = (item) => {
-    console.log(url,data,"data",item);
     setLoading(true)
     POST(url, data??item??{})
       .then((data) => { setApiData(data); setLoading(false); console.log(data); })
