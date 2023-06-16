a14304f569b0f59cc57b5615cbf2359c32c8ba6b
Sonali.Jain <sonali.jain@blackngreen.com>
2023-06-16 10:00:53 +0530

    view result file changes

--- src/Component/Swipe4win/ViewResult.jsx
+++ src/Component/Swipe4win/ViewResult.jsx
@@ -7,6 +7,7 @@ import { GET } from "../../shared/Axios";
 import clsx from "clsx";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import "tailwindcss/tailwind.css";
+import { Header } from "../../CommonComponent";
 const useStyles = makeStyles((theme) => ({
 
 
@@ -112,11 +113,20 @@ const ViewResult = () => {
     console.log(startDate, endDate);
     GET(`https://swip4winapiv1.bngrenew.com:5081/swipe4win/cms/report/leaderboard?operatorId=zainlibyana_libya&gtDate=${startDate?.toISOString().split("T")[0]}&ltDate=${endDate?.toISOString().split("T")[0]}`).then((res) => setReportData(res)).catch((err) => console.log(err))
   };
-const handleExport=()=>{
-  GET(`https://swip4winapiv1.bngrenew.com:5081/swipe4win/cms/report/export/excel?operatorId=zainlibyana_libya&gtDate=${startDate?.toISOString().split("T")[0]}&ltDate=${endDate?.toISOString().split("T")[0]}`).then((res)=>console.log(res)).catch((err)=>console.log(err))
-}
+  const handleExport = () => {
+    GET(`https://swip4winapiv1.bngrenew.com:5081/swipe4win/cms/report/export/excel?operatorId=zainlibyana_libya&gtDate=${startDate?.toISOString().split("T")[0]}&ltDate=${endDate?.toISOString().split("T")[0]}`).then((res) => res.blob()).then((blob) => {
+      const fileURL = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = fileURL;
+      console.log(fileURL,"fileURL");
+      link.download = 'downloaded_file'; // Replace with the desired file name
+      link.click()
+    }).catch((err) => console.log(err,"err"))
+  }
   return (
     <div className="mx-4 my-2">
+      <Header />
+      <div className="mt-20">
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Grid container spacing={4} alignItems="center">
           <Grid item xs={12} sm={3}>
@@ -194,65 +204,66 @@ const handleExport=()=>{
           <Grid item xs={12} sm={3}>
             <Button variant="outlined" onClick={handleSubmit}>Submit</Button>
           </Grid>
-          <Grid item xs={12} sm={3}>
+          {reportData!=={} && <Grid item xs={12} sm={3}>
             <Button variant="outlined" onClick={handleExport}>Export</Button>
-          </Grid>
+          </Grid>}
         </Grid>
       </MuiPickersUtilsProvider>
-      {reportData && <TableContainer className={(classes.tableContainer, "mt-20")}>
+      {reportData?.winners?.length > 0 && <TableContainer className={(classes.tableContainer, "mt-20")}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead className={classes.header}>
             <TableRow>
               <TableCell>S.No.</TableCell>
-              
-                <TableCell>portalAccessCount</TableCell>
-                <TableCell>rank</TableCell>
-                <TableCell>score</TableCell>
-                <TableCell>total Correct Answers</TableCell>
-                <TableCell>total Question Attempted</TableCell>
-            
+
+              <TableCell>portalAccessCount</TableCell>
+              <TableCell>rank</TableCell>
+              <TableCell>score</TableCell>
+              <TableCell>total Correct Answers</TableCell>
+              <TableCell>total Question Attempted</TableCell>
+
               <TableCell>Action</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-           {reportData?.winners?.length>0?
-             reportData.winners.map((item,idx)=>{
-               return(<TableRow key={item.rank}>
-                <TableCell>{idx + 1}</TableCell>
-                <TableCell key={item.rank}>{item.portalAccessCount}</TableCell>
-                <TableCell>{item.rank}</TableCell>
-                <TableCell>{item.score}</TableCell>
-                <TableCell>{item.totalCorrectAnswers}</TableCell>
-                <TableCell>{item.totalQuestionAttempted}</TableCell>
-                <IconButton
-                  aria-label="more"
-                  aria-controls="menu"
-                  aria-haspopup="true"
-                  className={classes.actionButton}
-                  onClick={(event) => handleMenuClick(event, item.rank)}
-                >
-                  <MoreVertIcon />
-                </IconButton>
-                <Menu
-                  id="menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl && selectedItemId === item.rank)}
-                  onClose={handleMenuClose}
-                  className={classes.menu}
-                >
-                  {/* onClick={()=>handleEdit(item,idx)} */}
-                  <MenuItem onClick={() => navigate("/swipe4win/EditDetails", { state: item })}>Edit</MenuItem>
-                  <MenuItem onClick={() => handleDelete(item)}>Delete</MenuItem>
-                </Menu>
+            {reportData?.winners?.length > 0 ?
+              reportData.winners.map((item, idx) => {
+                return (<TableRow key={item.rank}>
+                  <TableCell>{idx + 1}</TableCell>
+                  <TableCell key={item.rank}>{item.portalAccessCount}</TableCell>
+                  <TableCell>{item.rank}</TableCell>
+                  <TableCell>{item.score}</TableCell>
+                  <TableCell>{item.totalCorrectAnswers}</TableCell>
+                  <TableCell>{item.totalQuestionAttempted}</TableCell>
+                  <IconButton
+                    aria-label="more"
+                    aria-controls="menu"
+                    aria-haspopup="true"
+                    className={classes.actionButton}
+                    onClick={(event) => handleMenuClick(event, item.rank)}
+                  >
+                    <MoreVertIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl && selectedItemId === item.rank)}
+                    onClose={handleMenuClose}
+                    className={classes.menu}
+                  >
+                    {/* onClick={()=>handleEdit(item,idx)} */}
+                    <MenuItem onClick={() => navigate("/swipe4win/EditDetails", { state: item })}>Edit</MenuItem>
+                    <MenuItem onClick={() => handleDelete(item)}>Delete</MenuItem>
+                  </Menu>
 
-              </TableRow>)
-             })
-           :<div>no data found</div>}
-        
+                </TableRow>)
+              })
+              : <div>no data found</div>}
+
           </TableBody>
         </Table>
       </TableContainer>}
+      </div>
     </div>
   );
 };
