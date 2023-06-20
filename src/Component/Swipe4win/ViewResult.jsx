5e7e157f93a7795a91465b56f390bd913b072a1b
BNG <bng@BNGs-MacBook-Pro.local>
2023-06-20 17:01:02 +0530

    working on design part

--- src/Component/Swipe4win/ViewResult.jsx
+++ src/Component/Swipe4win/ViewResult.jsx
@@ -171,41 +171,7 @@ const ViewResult = () => {
               <Typography variant="subtitle1" style={{ color: "black" }}>
                 End Date
               </Typography>
-              <DatePicker
-                disableToolbar
-                variant="inline"
-                format="MM-dd-yyyy"
-                margin="normal"
-                id="end-date-picker"
-                value={endDate}
-                onChange={handleEndDateChange}
-                open={openEndDatePicker}
-                onOpen={() => setOpenEndDatePicker(true)}
-                onClose={() => setOpenEndDatePicker(false)}
-                TextFieldComponent={(props) => (
-                  <OutlinedInput
-                    {...props}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <CalendarTodayIcon
-                          style={{ color: "black" }}
-                          onClick={handleEndDateIconClick}
-                        />
-                      </InputAdornment>
-                    }
-                  />
-                )}
-                inputProps={{
-                  readOnly: true, // Make the input field readOnly
-                }}
-              />
-           
-           
-          
- 
-
-
-              {/* <KeyboardDatePicker
+              <KeyboardDatePicker
                 disableToolbar
                 variant="inline"
                 format="MM-dd-yyyy"
@@ -235,7 +201,7 @@ const ViewResult = () => {
                 keyboardInputProps={{
                   readOnly: true, // Make the input field readOnly
                 }}
-              /> */}
+              />
             </Grid>
             <Grid item xs={12} sm={3}>
               <Button variant="outlined" className={classes.submit} onClick={handleSubmit}>Submit</Button>
